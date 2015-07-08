36916304dd574e6b0397d73e7fd63624df83a1d1
anggi <anggraita.laras@gmail.com>
2015-07-08 09:36:29 +0700

    its

--- app/burger.php
+++ app/burger.php
@@ -1,21 +1,19 @@
-<div class="nav-burger mobile-only">
-        <div class="bit-12 container-burger">
-          <div class="menu-burger">
-            <i class="icon-menu"></i>
+        <div class="bit-2 ico-burger mobile-only">
+          <div class="menu-burger"> test
+            <span class="icon-menu"></span>
           </div>
         </div>
 
         <div class="bit-1 burger">
           <div class="nav-burger-close"></div>
             <nav class="nav-burger">
-            <ul>  
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Food Rating</a></li>
-              <li><a href="#">Highest Rating</a></li>
-              <li><a href="#">Tips</a></li>
-              <li><a href="#">Review</a></li>
-              <li><a href="#">Contact</a></li>
+              <ul>  
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Food Rating</a></li>
+                <li><a href="#">Highest Rating</a></li>
+                <li><a href="#">Tips</a></li>
+                <li><a href="#">Review</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
-            </nav>
+          </nav>
         </div>
-      </div>
